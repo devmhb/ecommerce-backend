15c7f768dfc02b5b089b91835a6846cfffb93094
hrshainik <hrsshainik@gmail.com>
2021-05-30 18:36:28 +0600

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b07178d965ef541d274e4de7629a1f9e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '981f3de3-b689-4170-b36f-d0c989e91e7c'
+};
